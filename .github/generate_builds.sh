38aee85e29e45dc91f441fa14d0efd97c1824fce
BJMEJIA <57636051+BJMEJIA@users.noreply.github.com>
2024-10-16 15:37:12 -0600

    Update generate_builds.sh

--- .github/generate_builds.sh
+++ .github/generate_builds.sh
@@ -6,7 +6,7 @@ clear_repo(){
 
 cd "$REPO_DIR"
 
-NEED_TO_BUILD=($(echo '$SELECTED_VERSIONS' | jq -r 'to_entries | map(select(.value == true )) | .[].key')))
+NEED_TO_BUILD=($(echo '$SELECTED_VERSIONS' | jq -r 'to_entries | map(select(.value == true )) | .[].key'))
 
 echo "${NEED_TO_BUILD[@]}"
 
